bf17ecf4869b6b8002986138c90e7e26d9e12a44
Alexander Katkov <afargoth@gmail.com>
2022-08-28 18:43:17 +0700

    [CSHW-6]: Add filter MoviesOnly

--- MovieActorSearch/Application/MovieActorSearchService.cs
+++ MovieActorSearch/Application/MovieActorSearchService.cs
@@ -7,6 +7,9 @@ namespace MovieActorSearch.Application;
 
 public class MovieActorSearchService : IMovieActorSearchService
 {
+    private const string RoleActor = "Actor";
+    private const string RoleActress = "Actress";
+    
     private readonly IDbProvider _dbProvider;
     private readonly IApiProvider _apiProvider;
 
@@ -18,7 +21,7 @@ public class MovieActorSearchService : IMovieActorSearchService
     
     public async Task<MatchResponse> MatchActors(MatchRequest request, CancellationToken ct)
     {
-        var movies1 = await GetActorMovies(request.Actor1, ct);
+        var movies1 = await GetActorMovies(request.Actor1, request.MoviesOnly, ct);
         
         IEnumerable<Movie> commonMovies;
         if (request.Actor1 == request.Actor2)
@@ -27,29 +30,28 @@ public class MovieActorSearchService : IMovieActorSearchService
         }
         else
         {
-            var movies2 = await GetActorMovies(request.Actor2, ct);
+            var movies2 = await GetActorMovies(request.Actor2, request.MoviesOnly, ct);
             commonMovies = Movie.Intersect(movies1, movies2);
         }
         
-        // todo: search MoviesOnly
-        //if (request.MoviesOnly == true)
-        //{
-        //    movs1 = movs1.Where(m => m.Role == "Actress" || m.Role == "Actor").ToArray();
-        //    movs1 = movs1.Where(m => m.Role == "Actress" || m.Role == "Actor").ToArray();
-        //}
-
         var result = commonMovies.Select(x => x.Title)
             .OrderBy(x => x);
         
         return new MatchResponse(result);
     }
 
-    private async Task<Movie[]> GetActorMovies(string actorName, CancellationToken ct)
+    private async Task<Movie[]> GetActorMovies(string actorName, bool moviesOnly, CancellationToken ct)
     {
         var actor = await GetActor(actorName, ct);
-        var movies = await _apiProvider.FindActorMovies(actor.Id, ct);
+        var movies = (await _apiProvider.FindActorMovies(actor.Id, ct))?.CastMovies;
+
+        if (movies is null)
+            return Array.Empty<Movie>();
         
-        return movies is null ? Array.Empty<Movie>() : movies.CastMovies;
+        if (moviesOnly)
+            movies = movies.Where(x => x.Role is RoleActor or RoleActress).ToArray();
+
+        return movies;
     }
     
     private async Task<Actor> GetActor(string name, CancellationToken ct)
